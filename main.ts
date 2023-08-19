d1d27f09e0ae95ca9f0095d8a9efba5e2f87c69d
zx6r000 <140099462+zx6r000@users.noreply.github.com>
2023-08-19 18:04:37 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.showIcon(IconNames.Surprised)
+makerbit.connectLcd(32)
+basic.pause(1000)
+makerbit.showStringOnLcd1602("MakerBit", makerbit.position1602(LcdPosition1602.Pos1), 16, TextOption.AlignLeft)
+basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+	
+})
